63bb1a6b27cfffec3d8580eabd77d4845ab11ce0
JUHEE <94836793+wngml02@users.noreply.github.com>
2022-03-18 23:40:56 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
-print("HELLO WORLD!")
-print("박주희_202101533_컴퓨터공학부")
-print("정보")
+print("HELLO WORLD!") #HELLO WORLD 출력
+print("박주희_202101533_컴퓨터공학부") #내 정보 출력
+print("정보") #좋아하는 과목 출력
 print("Hufs Computer Engineering")
 
-n = int(input())
+
+#입력된 n만큼 'computer'출력하기(n>2)
+n = int(input()) #n입력받기
 if n > 2:
 	for i in range(n):
-		print("computer")
+		print("computer") #입력 받은 n만큼 print 반복하기
 	
 else:
-	print("3이상 입력하세요")
+	print("3이상 입력하세요") #2이상이 아닌 경우
